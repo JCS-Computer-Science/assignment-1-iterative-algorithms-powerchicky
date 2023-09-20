f4dc0d285e33b24b8b48368355a2a20464d43265
Averie Atwood <bowenarw@gmail.com>
2023-09-19 22:41:54 -0600

    finished selection and insertion sort algs

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,11 +1,20 @@
 function insertionSort(array) {
-	/*
+    /*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
 	 ** is bigger
 	 */
-
-	return array;
+    let n = array.length;
+        for (let i = 1; i < n; i++) {
+            let current = array[i];
+            let j = i-1; 
+            while ((j > -1) && (current < array[j])) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
+    return array;
 }
 
 module.exports = insertionSort;
--- selection-sort.js
+++ selection-sort.js
@@ -1,4 +1,4 @@
-function selectionSort(array) {
+function selectionSort(array) { 
 	/*
 	 ** Search through the whole array for the smallest element
 	 ** then swap it with the first element.
@@ -6,8 +6,24 @@ function selectionSort(array) {
 	 ** for the smallest, then swap it with the second element.
 	 ** Continue like this until you have only one element left.
 	 */
-
-	return array;
-}
+		let n = array.length;
+			
+		for(let i = 0; i < n; i++) {
+			// Finding the smallest number in the subarray
+			let min = i;
+			for(let j = i+1; j < n; j++){
+				if(array[j] < array[min]) {
+					min=j; 
+				}
+			 }
+			 if (min != i) {
+				 // Swapping the elements
+				 let tmp = array[i]; 
+				 array[i] = array[min];
+				 array[min] = tmp;      
+			}
+		}
+		return array;
+	}
 
 module.exports = selectionSort;
